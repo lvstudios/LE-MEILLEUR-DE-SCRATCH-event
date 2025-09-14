87fe51c7c53293e0ccf81e5f2639331a5d8ce532
lvstudios <137897787+lvstudios@users.noreply.github.com>
2025-09-14 11:39:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Popup
-window.addEventListener('DOMContentLoaded', () => {
-  const popup = document.getElementById('popup-info');
-  const closeBtn = document.getElementById('close-popup');
-  closeBtn.addEventListener('click', () => {
-    popup.style.display = 'none';
+window.addEventListener('DOMContentLoaded',()=>{
+  const popup=document.getElementById('popup-info');
+  const closeBtn=document.getElementById('close-popup');
+  closeBtn.addEventListener('click',()=>{
+    popup.style.display='none';
     document.querySelector('main').scrollIntoView({behavior:'smooth'});
   });
 });
